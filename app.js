fd12f7de5cac1d1a5024fa26d048ddfc83062c6e
ChloeLiang <nliangxin@gmail.com>
2018-11-09 10:33:55 +0800

    Search by any attribute

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const jsonfile = require('jsonfile');
 const file = 'phonebook.json';
 
+// Search a list of string
+
 const binarySearch = (list, searchValue) => {
   let low = 0;
   let high = list.length;
@@ -36,6 +38,8 @@ const nameList = [
 console.log(binarySearch(nameList, 'Daisy'));
 console.log(binarySearch(nameList, 'Bruce'));
 
+// Search with a list of objects
+
 const binarySearchById = (list, searchId) => {
   let low = 0;
   let high = list.length;
@@ -61,4 +65,49 @@ jsonfile.readFile(file, (err, obj) => {
   }
 
   console.log(binarySearchById(obj, 44));
+});
+
+// Search a list of objects by any attribute
+
+const sortByAttribute = (list, attribute) => {
+  return list.sort((a, b) => {
+    if (a[attribute] < b[attribute]) {
+      return -1;
+    }
+
+    if (a[attribute] > b[attribute]) {
+      return 1;
+    }
+
+    return 0;
+  });
+};
+
+const binarySearchByAttribute = (list, attribute, value) => {
+  list = sortByAttribute(list, attribute);
+
+  let low = 0;
+  let high = list.length;
+
+  while (low <= high) {
+    let middle = Math.floor((low + high) / 2);
+
+    if (list[middle][attribute] === value) {
+      return list[middle];
+    } else if (list[middle][attribute] < value) {
+      low = middle + 1;
+    } else if (list[middle][attribute] > value) {
+      high = middle - 1;
+    }
+  }
+
+  return false;
+};
+
+jsonfile.readFile(file, (err, obj) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(binarySearchByAttribute(obj, 'name', 'Micheal'));
 });
